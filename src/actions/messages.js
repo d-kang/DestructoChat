e52972ca6417f54ad7498b0e7e87c30c5e4cd5a1
d-kang <david.kang714@gmail.com>
2018-02-18 12:35:55 -0800

    Add /reducers/messages.js

---
+++ src/actions/messages.js
@@ -0,0 +1,40 @@
+import {
+  LOAD_MESSAGES_REQUEST,
+  LOAD_MESSAGES_SUCCESS,
+  LOAD_MESSAGES_FAILURE,
+} from './constants';
+
+import mockMessages from '../data/mockMessages';
+
+const promisify = () =>
+  new Promise(resolve => {
+    setTimeout(() => resolve(mockMessages), 1000);
+  });
+
+const loadMessagesRequest = chatroom => ({
+  type: LOAD_MESSAGES_REQUEST,
+  logger: console.log('chatroom2', chatroom),
+  chatroom,
+});
+const loadMessagesSuccess = payload => ({
+  type: LOAD_MESSAGES_SUCCESS,
+  payload,
+});
+const loadMessagesFailure = error => ({
+  type: LOAD_MESSAGES_FAILURE,
+  payload: error,
+});
+
+const loadUsersMessages = chatroom => dispatch => {
+  dispatch(loadMessagesRequest(chatroom));
+  return promisify(chatroom)
+    .then(data => dispatch(loadMessagesSuccess(data)))
+    .catch(error => dispatch(loadMessagesFailure(error)));
+};
+
+export {
+  loadUsersMessages,
+  loadMessagesRequest,
+  loadMessagesSuccess,
+  loadMessagesFailure,
+};
