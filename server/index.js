1f0dd80a733294900ebd8cae9e35ed6b37c5b321
d-kang <david.kang714@gmail.com>
2018-02-18 13:37:56 -0800

    Update server/index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+const server = require('./app');
 
-const app = (module.exports = express()); // eslint-disable-line no-multi-assign
+const PORT = process.env.PORT || 4242;
 
-app.set('port', process.env.PORT || 9090);
-const PORT = app.get('port');
-app.use(cors());
-app.use(express.static(path.join(__dirname, '../dist')));
-app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
+console.log('PORT', PORT);
+server.listen(PORT, () => {
+  console.log(`listening on http:localhost:${PORT}`);
+});
