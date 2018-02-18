13f2e4ac43e5715c3d987e643e77eb5a0b60fffc
d-kang <david.kang714@gmail.com>
2018-02-17 20:20:50 -0800

    Add server/index.js

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = (module.exports = express());
+
+app.set('port', process.env.PORT || 9090);
+const PORT = app.get('port');
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../dist')));
+app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
