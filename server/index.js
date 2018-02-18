6121387a931d3e0d2cf855f1e6a12bb039d04aa3
d-kang <david.kang714@gmail.com>
2018-02-17 23:59:43 -0800

    Add eslint exception to server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
-const app = (module.exports = express());
+const app = (module.exports = express()); // eslint-disable-line no-multi-assign
 
 app.set('port', process.env.PORT || 9090);
 const PORT = app.get('port');
