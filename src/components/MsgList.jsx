9c01bb9d7e2bbf3d514473f6b731d93d3941ac2d
d-kang <david.kang714@gmail.com>
2018-02-18 12:38:40 -0800

    Adjust code style in MsgList.jsx

--- src/components/MsgList.jsx
+++ src/components/MsgList.jsx
@@ -2,7 +2,9 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import MsgItem from './MsgItem';
 
-const MsgList = ({ messages }) => <div>{messages.map((msg, i) => <MsgItem key={i} {...msg} />)}</div>;
+const MsgList = ({ messages }) => (
+  <div>{messages.map((msg, i) => <MsgItem key={i} {...msg} />)}</div>
+);
 
 MsgList.propTypes = {
   messages: PropTypes.array.isRequired,
