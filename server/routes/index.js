0e98dac49f1d176209118b30a7801609bd555033
d-kang <david.kang714@gmail.com>
2018-02-19 14:18:20 -0800

    Remove subscribeToTimer from index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,12 +6,6 @@ const {
 } = require('../controllers/messageController');
 
 module.exports = socket => {
-  socket.on('subscribeToTimer', interval => {
-    setInterval(() => {
-      socket.emit('timer', new Date());
-    }, interval);
-  });
-
   socket.on('signup', data => signup(data, socket));
 
   socket.on('login', data => login(data, socket));
