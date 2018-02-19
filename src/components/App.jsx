5795055aed66f4992f1530b191a692e8fab84d73
d-kang <david.kang714@gmail.com>
2018-02-19 14:19:55 -0800

    Remove timestamp and loadUserMessages logic from App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,17 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import moment from 'moment';
 import LoginPage from './LoginPage';
 import ChatPage from './ChatPage';
-import { loadUsersMessages } from '../actions/messages';
-import { loadTimestamp } from '../actions/sockets';
 
 class App extends PureComponent {
-  componentDidMount() {
-    this.props.loadTimestamp();
-    this.props.loadUsersMessages('myAwesomeChatRoom');
-  }
+  componentDidMount() {}
 
   render() {
-    const { username, loggedIn, messages, timestamp, error } = this.props;
+    const { username, loggedIn, messages, error } = this.props;
     return (
       <div>
         <div>
-          <p>
-            This is the timer value:{' '}
-            {moment(timestamp).format('MMMM Do YYYY, h:mm:ss a')}
-          </p>
           {username ? <p>{`Username: ${username}`}</p> : <p>{error}</p>}
         </div>
         <div>
@@ -39,9 +29,6 @@ App.propTypes = {
   username: PropTypes.string.isRequired,
   loggedIn: PropTypes.bool.isRequired,
   messages: PropTypes.array.isRequired,
-  loadUsersMessages: PropTypes.func.isRequired,
-  timestamp: PropTypes.string.isRequired,
-  loadTimestamp: PropTypes.func.isRequired,
   error: PropTypes.string,
 };
 
@@ -51,10 +38,6 @@ const mapStateToProps = ({ login, messages, socket }) => ({
   loggedIn: login.loggedIn,
   error: login.error,
   messages: messages.messages,
-  loadUsersMessages: messages.loadUsersMessages,
-  timestamp: socket.timestamp,
 });
 
-export default connect(mapStateToProps, { loadUsersMessages, loadTimestamp })(
-  App
-);
+export default connect(mapStateToProps)(App);
