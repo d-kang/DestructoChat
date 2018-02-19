f156cb1637581b41adbad1e60dbdc49cec2094a8
d-kang <david.kang714@gmail.com>
2018-02-18 20:29:09 -0800

    Username thunks with socketio to /actions/username.js

--- src/actions/username.js
+++ src/actions/username.js
@@ -1,5 +1,37 @@
-import { SET_USERNAME } from './constants';
+import socket from '../socketConnection';
+import {
+  LOGIN_USERNAME_REQUEST,
+  LOGIN_USERNAME_SUCCESS,
+  LOGIN_USERNAME_FAILURE,
+  SIGNUP_USERNAME_REQUEST,
+  SIGNUP_USERNAME_SUCCESS,
+  SIGNUP_USERNAME_FAILURE,
+} from './constants';
 
-const setUsername = payload => ({ type: SET_USERNAME, payload });
+const loginUserRequest = payload => dispatch => {
+  socket.emit('login', payload);
+  dispatch({ type: LOGIN_USERNAME_REQUEST });
+  socket.on('login', data => {
+    console.log('on login >> loginUserRequest', data);
+    if (data.username !== null) {
+      dispatch({ type: LOGIN_USERNAME_SUCCESS, payload: data.username });
+    } else {
+      dispatch({ type: LOGIN_USERNAME_FAILURE, error: data.error });
+    }
+  });
+};
 
-export { setUsername };
+const signupUserRequest = payload => dispatch => {
+  socket.emit('signup', payload);
+  dispatch({ type: SIGNUP_USERNAME_REQUEST });
+  socket.on('signup', data => {
+    console.log('on login >> loginUserRequest', data);
+    if (data.username !== null) {
+      dispatch({ type: SIGNUP_USERNAME_SUCCESS, payload: data.username });
+    } else {
+      dispatch({ type: SIGNUP_USERNAME_FAILURE, error: data.error });
+    }
+  });
+};
+
+export { loginUserRequest, signupUserRequest };
