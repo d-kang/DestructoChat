25812c0137ef05860d488fa039f40b93c3301dd7
d-kang <david.kang714@gmail.com>
2018-02-19 14:21:17 -0800

    Remove socket from Root Reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import login from './login';
 import messages from './messages';
-import socket from './socket';
 
 export default combineReducers({
   login,
   messages,
-  socket,
 });
