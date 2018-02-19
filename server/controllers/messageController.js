31e13627b762177b6878736873e4e83b3684226a
d-kang <david.kang714@gmail.com>
2018-02-19 11:20:09 -0800

    Add destructable chat logic to messageController.js

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -2,17 +2,34 @@ const mongoose = require('mongoose');
 
 const Message = mongoose.model('Message');
 
+const destroyMessage = (_id, socket) => {
+  Message.find({ _id }).remove((err, data) =>
+    exports.updateMessages(err, data, socket)
+  );
+};
+
 exports.addMessage = (message, socket) => {
-  Message.create(message);
+  Message.create(message, (err, data) => {
+    if (err) {
+      console.error('err2', err);
+    } else if (data.selfDestruct === true) {
+      console.log('selfDestruct True >> data >>', data.message);
+      const timeTillDestruct = data.destructAt - Date.now();
+      console.log('timeTillDestruct', timeTillDestruct);
+      setTimeout(() => destroyMessage(data._id, socket), timeTillDestruct);
+    }
+  });
 };
 
 exports.loadMessages = (message, socket) => {
+  console.log('loadMessages ran');
   Message.find({}, (err, data) => {
     if (err) {
-      console.error('err', err);
+      console.error('err3', err);
     }
   }).then(data => {
     if (data.length !== 0) {
+      console.log('i ran', data);
       socket.emit('load messages', data);
     } else {
       socket.emit('load messages', {
@@ -20,4 +37,24 @@ exports.loadMessages = (message, socket) => {
       });
     }
   });
+};
+
+exports.updateMessages = (error, d, socket) => {
+  if (error) {
+    console.error('updateMessages error', error);
+  } else {
+    Message.find({}, err => {
+      if (err) {
+        console.error('error', err);
+      }
+    }).then(data => {
+      if (data.length !== 0) {
+        socket.broadcast.emit('load messages', data);
+      } else {
+        socket.emit('load messages', {
+          error: { errorMessage: 'Sorry No Messages.' },
+        });
+      }
+    });
+  }
 };
