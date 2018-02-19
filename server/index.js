98ce90ac97f0b2b4b080fea253747d093f6e1a1f
d-kang <david.kang714@gmail.com>
2018-02-18 20:26:20 -0800

    Add mongoose connection and env vars to /server/index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,20 @@
+const mongoose = require('mongoose');
+
+try {
+  require('dotenv').config({ path: 'variables.env' });
+} catch (e) {
+  console.error('e', e);
+}
+
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', error => {
+  console.error('mongoose connection error', error);
+});
+
+require('./models/User');
+require('./models/Message');
+
 const server = require('./app');
 
 const PORT = process.env.PORT || 8000;
