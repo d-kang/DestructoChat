2e90c498db60c8ced00125671b805d7e9f4d363c
d-kang <david.kang714@gmail.com>
2018-02-18 16:26:49 -0800

    Add socket connection to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,42 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import LoginPage from './LoginPage';
 import ChatPage from './ChatPage';
 import { setUsername } from '../actions/username';
 import { loadUsersMessages } from '../actions/messages';
+import { loadTimestamp } from '../actions/sockets';
+
+import socketIO from '../socketConnection';
 
 class App extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    socketIO.emit('subscribeToTimer', 1000);
+  }
   componentDidMount() {
+    this.props.loadTimestamp(socketIO);
     this.props.loadUsersMessages('myAwesomeChatRoom');
   }
 
   render() {
-    const { username, isLoggedIn, messages, setUsername } = this.props;
+    const {
+      username,
+      isLoggedIn,
+      messages,
+      setUsername,
+      timestamp,
+    } = this.props;
     return (
       <div>
+        <div>
+          <p>
+            This is the timer value:{' '}
+            {moment(timestamp).format('MMMM Do YYYY, h:mm:ss a')}
+          </p>
+        </div>
         <div>
           {!isLoggedIn ? (
             <LoginPage setUsername={setUsername} />
@@ -36,15 +58,22 @@ App.propTypes = {
   messages: PropTypes.array.isRequired,
   setUsername: PropTypes.func.isRequired,
   loadUsersMessages: PropTypes.func.isRequired,
+  timestamp: PropTypes.string.isRequired,
+  loadTimestamp: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = ({ login, messages }) => ({
+const mapStateToProps = ({ login, messages, socket }) => ({
   username: login.username,
   isLoggedIn: login.isLoggedIn,
   messages: messages.messages,
   loadUsersMessages: messages.loadUsersMessages,
+  timestamp: socket.timestamp,
 });
 
-export default connect(mapStateToProps, { setUsername, loadUsersMessages })(
-  App
-);
+const mapDispatchToProps = dispatch => ({
+  setUsername: username => dispatch(setUsername(username)),
+  loadUsersMessages: chatroom => dispatch(loadUsersMessages(chatroom)),
+  loadTimestamp: openSocket => dispatch(loadTimestamp(openSocket)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
