4b9222dd54910774552efef6327a1816782f8d4d
d-kang <david.kang714@gmail.com>
2018-02-18 16:27:09 -0800

    Add socket reducer to combine reducers

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import login from './login';
 import messages from './messages';
+import socket from './socket';
 
 export default combineReducers({
   login,
   messages,
+  socket,
 });
