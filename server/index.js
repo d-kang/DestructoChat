613404643d07d213877afe2325294737ca05b474
d-kang <david.kang714@gmail.com>
2018-02-18 22:36:38 -0800

    Abstract db connection

--- server/index.js
+++ server/index.js
@@ -1,20 +1,4 @@
-const mongoose = require('mongoose');
-
-try {
-  require('dotenv').config({ path: 'variables.env' });
-} catch (e) {
-  console.error('e', e);
-}
-
-mongoose.connect(process.env.DATABASE);
-mongoose.Promise = global.Promise;
-mongoose.connection.on('error', error => {
-  console.error('mongoose connection error', error);
-});
-
-require('./models/User');
-require('./models/Message');
-
+require('../db/dbConnection');
 const server = require('./app');
 
 const PORT = process.env.PORT || 8000;
