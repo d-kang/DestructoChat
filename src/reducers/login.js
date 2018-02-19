f410ffa248932d84029ab97b4eb2e178a30feeec
d-kang <david.kang714@gmail.com>
2018-02-18 20:30:30 -0800

    Update login reducer

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -1,15 +1,51 @@
 import { createReducer } from './createReducer';
-import { SET_USERNAME } from '../actions/constants';
+import {
+  LOGIN_USERNAME_REQUEST,
+  LOGIN_USERNAME_SUCCESS,
+  LOGIN_USERNAME_FAILURE,
+  SIGNUP_USERNAME_REQUEST,
+  SIGNUP_USERNAME_SUCCESS,
+  SIGNUP_USERNAME_FAILURE,
+} from '../actions/constants';
 
 const initialState = {
   username: '',
-  isLoggedIn: false,
+  loggedIn: false,
+  loading: false,
 };
 
 export default createReducer(initialState, {
-  [SET_USERNAME]: (state, payload) => ({
+  [LOGIN_USERNAME_REQUEST]: state => ({
     ...state,
-    isLoggedIn: true,
+    loading: true,
+    error: false,
+  }),
+  [LOGIN_USERNAME_SUCCESS]: (state, { payload }) => ({
+    ...state,
+    loading: false,
+    loggedIn: true,
+    logger: console.log('reducer payload', payload),
     username: payload,
+  }),
+  [LOGIN_USERNAME_FAILURE]: (state, { error }) => ({
+    ...state,
+    loading: false,
+    error,
+  }),
+  [SIGNUP_USERNAME_REQUEST]: state => ({
+    ...state,
+    loading: true,
+    error: false,
+  }),
+  [SIGNUP_USERNAME_SUCCESS]: (state, { payload }) => ({
+    ...state,
+    loading: false,
+    loggedIn: true,
+    username: payload,
+  }),
+  [SIGNUP_USERNAME_FAILURE]: (state, { error }) => ({
+    ...state,
+    loading: false,
+    error,
   }),
 });
