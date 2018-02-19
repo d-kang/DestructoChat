6dc27e1058d0835324595903a76598095364f6f6
d-kang <david.kang714@gmail.com>
2018-02-18 16:24:26 -0800

    Create socket.io connection on express

--- server/app.js
+++ server/app.js
@@ -1,10 +1,19 @@
 const http = require('http');
 const path = require('path');
 const express = require('express');
-const socketIo = require('socket.io');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = (module.exports = http.createServer(app));
-const io = socketIo(server);
+const io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, '../dist')));
+
+io.on('connection', client => {
+  client.on('subscribeToTimer', interval => {
+    console.log('client is subscribing to timer with interval ', interval);
+    setInterval(() => {
+      client.emit('timer', new Date());
+    }, interval);
+  });
+});
