072c3251e45fe975905ca06a6b777a2c433560b1
d-kang <david.kang714@gmail.com>
2018-02-20 15:42:43 -0800

    Pass Username through MsgList to MsgItem

--- src/components/MsgList.jsx
+++ src/components/MsgList.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MsgItem from './MsgItem';
 
-const MsgList = ({ messages }) => (
+const MsgList = ({ messages, username }) => (
   <div id="chat__chatbox">
-    {messages.map(msg => <MsgItem key={msg.messageId} {...msg} />)}
+    {messages.map(msg => (
+      <MsgItem key={msg.messageId} username={username} {...msg} />
+    ))}
   </div>
 );
 
 MsgList.propTypes = {
   messages: PropTypes.arrayOf(PropTypes.object).isRequired,
+  username: PropTypes.string.isRequired,
 };
 
 export default MsgList;
