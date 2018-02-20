fb92c0883339f0796f97effeebb97fe17889c428
d-kang <david.kang714@gmail.com>
2018-02-20 11:28:08 -0800

    Add selfdestruct functionality to Dropdown Items

--- src/components/DropdownUI.jsx
+++ src/components/DropdownUI.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Dropdown } from 'semantic-ui-react';
 
-const DropdownUI = ({ tagOptions, text }) => (
+const DropdownUI = ({ tagOptions, text, setSelfDestruct }) => (
   <Dropdown upward text={text} multiple icon="filter">
     <Dropdown.Menu>
       <Dropdown.Menu scrolling>
         {tagOptions.map(option => (
-          <Dropdown.Item key={option.value} {...option} />
+          <Dropdown.Item
+            key={option.value}
+            {...option}
+            onClick={() => {
+              const data = {
+                selfDestruct: true,
+                destructAt: option.value,
+                dropdownDisplay: option.text,
+              };
+              if (option.value === -1) {
+                data.selfDestruct = false;
+                data.dropdownDisplay = 'Self Destruct';
+              }
+              setSelfDestruct(data);
+            }}
+          />
         ))}
       </Dropdown.Menu>
     </Dropdown.Menu>
   </Dropdown>
 );
+
+DropdownUI.propTypes = {
+  tagOptions: PropTypes.arrayOf(PropTypes.object).isRequired,
+  text: PropTypes.string.isRequired,
+};
 
 export default DropdownUI;
