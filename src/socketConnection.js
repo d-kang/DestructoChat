50a034a01672a2e6f911f5a35872f8100cebaf56
d-kang <david.kang714@gmail.com>
2018-02-20 14:51:35 -0800

    Use require syntax on socket connection so db can seed same connection

--- src/socketConnection.js
+++ src/socketConnection.js
@@ -1,5 +1,5 @@
-import io from 'socket.io-client';
+const io = require('socket.io-client');
 
 const socket = io.connect('https://destructochat.herokuapp.com/');
 
-export default socket;
+module.exports = socket;
