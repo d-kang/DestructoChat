1c3293f1092fc4b290ad2487ff4fe99ae6440693
d-kang <david.kang714@gmail.com>
2018-02-20 10:18:05 -0800

    Add options.js to hold dropDownTagOptions

---
+++ src/options.js
@@ -0,0 +1,44 @@
+const dropDownTagOptions = [
+  {
+    text: '10 seconds',
+    value: 10000,
+    label: { color: 'orange', empty: true, circular: true },
+  },
+  {
+    text: '30 seconds',
+    value: 30000,
+    label: { color: 'yellow', empty: true, circular: true },
+  },
+  {
+    text: '1 minute',
+    value: 60000,
+    label: { color: 'olive', empty: true, circular: true },
+  },
+  {
+    text: '15 minutes',
+    value: 900000,
+    label: { color: 'green', empty: true, circular: true },
+  },
+  {
+    text: '30 minutes',
+    value: 1800000,
+    label: { color: 'teal', empty: true, circular: true },
+  },
+  {
+    text: '1 hour',
+    value: 3600000,
+    label: { color: 'blue', empty: true, circular: true },
+  },
+  {
+    text: '1 day',
+    value: 86400000,
+    label: { color: 'violet', empty: true, circular: true },
+  },
+  {
+    text: '1 week',
+    value: 604800000,
+    label: { color: 'purple', empty: true, circular: true },
+  },
+];
+
+export { dropDownTagOptions };
