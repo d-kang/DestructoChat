6cae1f8a4cabaea1b99bad88759ca074bc741aa0
d-kang <david.kang714@gmail.com>
2018-02-19 16:51:22 -0800

    Add style.css to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import App from './components/App';
+import './style.scss';
 
 const store = configureStore();
 
