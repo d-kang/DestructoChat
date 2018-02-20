ef75ebee98eebbb833991ca718c77bd86c927ebf
d-kang <david.kang714@gmail.com>
2018-02-20 14:50:14 -0800

    Add socket.on 'delete messages' to routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const { signup, login } = require('../controllers/userController');
 const {
   addMessage,
   loadMessages,
+  deleteMessage,
 } = require('../controllers/messageController');
 
 module.exports = socket => {
@@ -13,4 +14,6 @@ module.exports = socket => {
   socket.on('add message', data => addMessage(data, socket));
 
   socket.on('load messages', () => loadMessages(socket));
+
+  socket.on('delete message', data => deleteMessage(data, socket));
 };
