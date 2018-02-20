f4161de62b181838ed97d4e497137441498ee9c5
d-kang <david.kang714@gmail.com>
2018-02-20 14:49:20 -0800

    Deconsole log index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const { server } = require('./app');
 
 const PORT = process.env.PORT || 8000;
 
-console.log('PORT', PORT);
 server.listen(PORT, () => {
   console.log(`listening on http:localhost:${PORT}`);
 });
