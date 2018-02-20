5f9c9c6c7faa3863dcaebe859f17a6b309122eb5
d-kang <david.kang714@gmail.com>
2018-02-19 18:52:17 -0800

    Add createdAt timestamps to Message Schema

--- db/models/Message.js
+++ db/models/Message.js
@@ -3,26 +3,29 @@ const autoIncrement = require('mongoose-auto-increment');
 
 mongoose.Promise = global.Promise;
 
-const messageSchema = new mongoose.Schema({
-  messageId: {
-    type: Number,
-    required: true,
+const messageSchema = new mongoose.Schema(
+  {
+    messageId: {
+      type: Number,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    selfDestruct: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    destructAt: Number,
   },
-  username: {
-    type: String,
-    required: true,
-  },
-  message: {
-    type: String,
-    required: true,
-  },
-  selfDestruct: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-  destructAt: Number,
-});
+  { timestamps: true }
+);
 
 messageSchema.plugin(autoIncrement.plugin, {
   model: 'Message',
