88cd54d57cca37e8142f3299741f8434c05273bb
d-kang <david.kang714@gmail.com>
2018-02-19 16:52:33 -0800

    Add semantic ui styled components to LoginPage

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,24 +1,29 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Message,
+  Segment,
+} from 'semantic-ui-react';
 import { loginUserRequest, signupUserRequest } from '../actions/username';
 
 class LoginPage extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      textInput: '',
-      select: 'Login',
-    };
-  }
-
+  state = {
+    textInput: '',
+    loginScreen: true,
+  };
   onChange = e => {
     this.setState({ textInput: e.target.value });
   };
-  onSubmit = e => {
+
+  onEnter = e => {
     e.preventDefault();
-    const { select, textInput } = this.state;
-    if (select === 'Login') {
+    const { loginScreen, textInput } = this.state;
+    if (loginScreen) {
       this.props.loginUserRequest(textInput);
     } else {
       this.props.signupUserRequest(textInput);
@@ -26,32 +31,87 @@ class LoginPage extends PureComponent {
     e.target[0].value = '';
   };
 
+  switchLoginSignup = e => {
+    e.preventDefault();
+    this.setState({ loginScreen: !this.state.loginScreen });
+  };
+
   handleSelection = e => {
     this.setState({ select: e.target.value });
   };
 
   render() {
+    let loginOrSignup;
+    if (this.state.loginScreen) {
+      loginOrSignup = this.props.onLogin;
+    } else {
+      loginOrSignup = this.props.onSignup;
+    }
+
+    const { topMsg, btnMsg, btmleftMsg, btmRightMsg } = loginOrSignup;
     return (
-      <div>
-        <select onChange={this.handleSelection}>
-          <option value="Login">Login</option>
-          <option value="Signup">Signup</option>
-        </select>
-        <form onSubmit={this.onSubmit} action="">
-          {this.state.select}:{' '}
-          <input type="text" maxLength="14" onChange={this.onChange} />
-        </form>
+      <div className="login-form">
+        <Grid
+          textAlign="center"
+          style={{ height: '100%' }}
+          verticalAlign="middle"
+        >
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as="h2" id="login__header" textAlign="center">
+              {topMsg}
+            </Header>
+            <Form size="large" onSubmit={this.onEnter}>
+              <Segment stacked>
+                <Form.Input
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Nickname"
+                  onChange={this.onChange}
+                />
+                <Button id="login__button" fluid size="large">
+                  {btnMsg}
+                </Button>
+              </Segment>
+            </Form>
+            <Message>
+              {this.props.error && <Message>{this.props.error}</Message>}
+              {btmleftMsg} <a onClick={this.switchLoginSignup}>{btmRightMsg}</a>
+            </Message>
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
 }
 
+LoginPage.defaultProps = {
+  error: '',
+  onLogin: {
+    topMsg: 'Log-in to your account',
+    btnMsg: 'Login',
+    btmleftMsg: 'New to us?',
+    btmRightMsg: 'Sign Up',
+  },
+  onSignup: {
+    topMsg: 'Sign-up for an account',
+    btnMsg: 'Signup',
+    btmleftMsg: 'Already a member?',
+    btmRightMsg: 'Log in',
+  },
+};
+
 LoginPage.propTypes = {
   loginUserRequest: PropTypes.func.isRequired,
   signupUserRequest: PropTypes.func.isRequired,
+  onLogin: PropTypes.object,
+  onSignup: PropTypes.object,
+  error: PropTypes.string,
 };
 
-const mapStateToProps = store => ({});
+const mapStateToProps = ({ login }) => ({
+  error: login.error,
+});
 
 export default connect(mapStateToProps, {
   loginUserRequest,
