2bb8de04f72371003f09bcbbff9435d4e369b2c2
d-kang <david.kang714@gmail.com>
2018-02-23 19:53:30 -0800

    Wrap MsgList in ChatScroll

--- src/components/MsgList.jsx
+++ src/components/MsgList.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import MsgItem from './MsgItem';
+import ChatScroll from './ChatScroll';
 
-const MsgList = ({ messages, username }) =>
-  messages.map(msg => (
-    <MsgItem key={msg.messageId} username={username} {...msg} />
-  ));
+class MsgList extends PureComponent {
+  render() {
+    const { messages, messageId, username, ...props } = this.props;
+    return (
+      <div id="chat__chatbox" {...props}>
+        {messages.map(msg => (
+          <MsgItem key={messageId} username={username} {...msg} />
+        ))}
+      </div>
+    );
+  }
+}
 
 MsgList.propTypes = {
   messages: PropTypes.arrayOf(PropTypes.object).isRequired,
   username: PropTypes.string.isRequired,
+  messageId: PropTypes.string.isRequired,
 };
 
-export default MsgList;
+export default ChatScroll(MsgList);
