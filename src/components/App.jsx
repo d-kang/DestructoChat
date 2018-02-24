0131fb01b963c20d6d673bf8bc41ead43f1c6378
d-kang <david.kang714@gmail.com>
2018-02-23 19:05:44 -0800

    Add loadMessages action to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import LoginPage from './LoginPage';
 import ChatContainer from './ChatContainer';
+import { loadUsersMessages } from '../actions/messages';
 
-const App = ({ loggedIn }) => (
-  <div>{!loggedIn ? <LoginPage /> : <ChatContainer />}</div>
-);
+class App extends PureComponent {
+  componentDidMount() {
+    this.props.loadMessages();
+  }
+  render() {
+    const { loggedIn } = this.props;
+    return <div>{!loggedIn ? <LoginPage /> : <ChatContainer />}</div>;
+  }
+}
 
 App.propTypes = {
   loggedIn: PropTypes.bool.isRequired,
@@ -16,4 +24,6 @@ const mapStateToProps = ({ login }) => ({
   loggedIn: login.loggedIn,
 });
 
-export default connect(mapStateToProps)(App);
+export default withRouter(
+  connect(mapStateToProps, { loadMessages: loadUsersMessages })(App)
+);
