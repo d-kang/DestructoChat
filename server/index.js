636d08918fa6d479dc8fcf5bd017b57244e83a72
d-kang <david.kang714@gmail.com>
2018-02-27 15:46:23 -0800

    Rename routes/index.js to server

--- server/index.js
+++
@@ -1,8 +0,0 @@
-require('../db/dbConnection');
-const { server } = require('./app');
-
-const PORT = process.env.PORT || 8000;
-
-server.listen(PORT, () => {
-  console.log(`listening on http://localhost:${PORT}`);
-});
