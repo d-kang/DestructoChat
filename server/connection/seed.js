3ac34b3876775625ad6d8af550af33bddf32a9a1
d-kang <david.kang714@gmail.com>
2018-02-27 15:47:58 -0800

    Update seed.js

---
+++ server/connection/seed.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { users, messages } = require('./seedData');
+
+const User = mongoose.model('User');
+const Message = mongoose.model('Message');
+
+const client = require('../../src/socketConnection');
+
+Message.remove({}, err => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Message Collection Cleared');
+    messages.forEach(message => {
+      client.emit('add message', message);
+    });
+  }
+});
